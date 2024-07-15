5a26ee388f180984d792cc983b4949329cb53885
Doganur <100204691+felixfelicisdgnr@users.noreply.github.com>
2024-07-15 15:58:04 +0300

    first commit

---
+++ src/main/java/org/example/Product.java
@@ -0,0 +1,16 @@
+package org.example;
+
+class Product {
+
+    String name;
+    double price;
+    int quantity;
+    double productScore;
+
+    public Product(String name, double price, int quantity, double productScore) {
+        this.name = name;
+        this.price = price;
+        this.quantity = quantity;
+        this.productScore = productScore;
+    }
+}
